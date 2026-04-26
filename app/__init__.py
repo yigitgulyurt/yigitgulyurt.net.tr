b9b6e027354a4d52f088766d361ac802f26bcbe8
yigitgulyurt <109045170+yigitgulyurt@users.noreply.github.com>
2026-04-26 18:18:21 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,8 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    app.config['SESSION_COOKIE_DOMAIN'] = '.yigitgulyurt.net.tr'
+
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
